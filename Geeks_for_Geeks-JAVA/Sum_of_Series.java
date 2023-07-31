0e1594e6808ac6cee42e0bb201dc92b4e91e810d
Dhruv <chodvadiyadhruv06@gmail.com>
2023-07-31 19:29:03 +0530

    Sum of series commit

--- Geeks_for_Geeks-JAVA/Sum_of_Series.java
+++ Geeks_for_Geeks-JAVA/Sum_of_Series.java
@@ -1,5 +1,4 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Sum_of_Series {
     public static void main(String[] args) {
         int n=5;
