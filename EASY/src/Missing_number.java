d5ea9f35e46c796b65da9bcfdaa80d356c27ef6f
Dhruv <chodvadiyadhruv06@gmail.com>
2023-09-17 11:52:48 +0530

    Missing number in array commit

---
+++ EASY/src/Missing_number.java
@@ -0,0 +1,17 @@
+public class Missing_number {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        int n=arr.length;
+        int val=missing(arr,n);
+    }
+    public static int missing(int[] array,int n){
+        int totalSum = n * (n + 1) / 2;
+        // Calculate the sum of elements in the array
+        int arraySum = 0;
+        for (int num : array) {
+            arraySum += num;
+        }
+        // The missing number will be the difference between the totalSum and arraySum
+        return totalSum - arraySum;
+    }
+}
